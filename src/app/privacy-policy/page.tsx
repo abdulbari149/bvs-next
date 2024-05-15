f27454fca61204240cfa621e4955181ac2310d62
abdulbari149 <abdulbari131103@gmail.com>
2024-05-16 00:49:41 +0500

    fix: privacy policy alignment

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -2,6 +2,7 @@ import PrivacyPolicy from "@/container/privacy-policy";
 import { generateSeo } from "@/utils/seo";
 import { Metadata } from "next";
 import "@/css/style.css";
+import "@/css/privacy-policy.css"
 
 export const metadata: Metadata = generateSeo("BVS | Privacy Policy");
 
